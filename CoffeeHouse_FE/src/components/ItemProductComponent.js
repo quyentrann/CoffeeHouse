fb711fc2d8cefaa8c79ac8d039b0a5064a9bb97f
quyentrann <dangthiquyentran22@gmail.com>
2024-11-10 21:34:11 +0700

    remove currency.js

--- CoffeeHouse_FE/src/components/ItemProductComponent.js
+++ CoffeeHouse_FE/src/components/ItemProductComponent.js
@@ -1,7 +1,6 @@
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
 import AntDesign from '@expo/vector-icons/AntDesign';
-import currency from 'currency.js';
 
 export default function ItemProductComponent({product}) {
   return (
